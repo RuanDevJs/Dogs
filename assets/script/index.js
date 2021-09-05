8159fdc5725e12450ede0ff0d713f8ea34d010cc
Ruan <ruanelpidio@hotmail.com>
2021-09-05 10:56:26 -0300

    new(): Project Dogs

---
+++ assets/script/index.js
@@ -0,0 +1,53 @@
+function AnimeIntro(){
+  const elementsAnime = document.querySelectorAll('[data-AnimeIntro]');
+
+  elementsAnime.forEach((el, index) => {
+    setTimeout(() => el.classList.add("activeAnimeIntro"), index * 100)
+  });
+}AnimeIntro();
+
+function AnimateScroll(){
+  const elementsAnime = document.querySelectorAll('[data-scroll]');
+
+  window.addEventListener("scroll", () => {
+    const distance = window.pageYOffset;
+
+    elementsAnime.forEach((el, index) => {
+      if(distance > el.offsetTop - 300){
+        setTimeout(() =>  el.classList.add("activeDataScroll"), index * 160);
+      }
+    });
+  })
+}AnimateScroll();
+
+function AnimateScrollRight(){
+  const elementsAnime = document.querySelectorAll('[data-scrollRight]');
+
+  window.addEventListener("scroll", () => {
+    const distance = window.pageYOffset;
+
+    elementsAnime.forEach((el, index) => {
+      if(distance > el.offsetTop - 300){
+        setTimeout(() =>  el.classList.add("activeRight"), index * 160);
+      }
+    });
+  })
+}AnimateScrollRight();
+
+let index = 0;
+
+function Slide(){
+  index++;
+
+  const dataSlider = document.querySelectorAll('[data-slide]');
+  
+  dataSlider.forEach(el => el.classList.remove("activeSlide"));
+
+  if(index > dataSlider.length - 1){
+    index = 0;
+  }
+
+  dataSlider[index].classList.add("activeSlide");
+
+  setTimeout(Slide, 4000)
+}Slide();
